e1f0cc2eb9de52a99c8ed44a281b7c14f9b9abe8
Jinhao Qiu <veiairn@gmail.com>
2024-03-03 09:39:11 +0900

    001-008

---
+++ ABC003_A.py
@@ -0,0 +1,4 @@
+n = int(input())
+
+res=int(((n+1)/2)*10000)
+print(res,end='\n')
---
+++ ABC006_A.py
@@ -0,0 +1,5 @@
+n=int(input())
+if ("3" in str(n) or n%3==0):
+    print("YES",end='\n')
+else:
+    print("NO",end='\n')
